e8ef8a6305aaf8776e01c228423e7d5a20db14e2
ngur <ngur@umich.edu>
2021-01-18 01:24:46 -0500

    Adds config.yml + updates to site

---
+++ index.js
@@ -0,0 +1,25 @@
+// javascript portions
+function enter(image) {
+    const val = document.getElementById(image);
+  
+    if (!val.hasAttribute('default')) {
+      val.setAttribute('default', val.getAttribute('src'));
+    } 
+    val.style.height = val.clientHeight + "px";
+    val.setAttribute('src', val.getAttribute('hover'));
+  }
+  
+  
+function leave(image) {
+const val = document.getElementById(image);
+val.setAttribute('src', val.getAttribute('default'));
+val.style.height = null;
+}
+  
+  
+  
+$(document).ready(function(){
+    $('[tool-tip-toggle="my-tooltip"]').tooltip({
+    placement : 'bottom'
+    });
+});
